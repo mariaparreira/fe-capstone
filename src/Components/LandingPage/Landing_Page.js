022988151acc8b1d4a49a84c5a3d9f6359db7fac
mariaparreira <minesparreira99@gmail.com>
2023-11-13 11:18:20 -0500

    updating Landing_Page and App.js

---
+++ src/Components/LandingPage/Landing_Page.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Landing_Page.css';
+
+const Landing_Page = () => {
+    return (
+        <section className="hero-section">
+            <div>
+                <div data-aos="fade-up" className="flex-hero">
+                    <h1>
+                        Your Health<br/>
+                        <span className="text-gradient">
+                        
+                        Our Responsibility
+                        </span>
+                    </h1>
+                    <div className="blob-cont">
+                        <div className="blue blob"></div>
+                    </div>
+                    <div className="blob-cont">
+                        <div className="pink blob"></div>
+                    </div>
+                    <div className="blob-cont">
+                        <div className="blue1 blob"></div>
+                    </div>
+                    <div className="blob-cont">
+                        <div className="pink1 blob"></div>
+                    </div>
+                    <h4>
+                        Wherever you are, whenever you need it, we are here to take your appointment! At the distance of a few clicks. As easy as it gets!
+                    </h4>
+                    <a href="#services">
+                        <button className="button">Get Started</button>
+                    </a>      
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Landing_Page;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './Navbar.css'
+import StayHealthyLogo from '../images/StayHealthyLogo.png';
+
+const Navbar = () => {
+
+    return(
+        <nav className="navbar navbar-expand-lg bg-white">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/"><b>StayHealthy</b><img src={StayHealthyLogo} className="d-inline-block align-top" alt="" /></Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav_item">
+                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                        </li>
+                        <li className="nav_item">
+                            <Link className="nav-link" to="/appointments">Appointments</Link>
+                        </li>
+                        <li className="nav_item">
+                            <Link className="nav-link" to="/reviews">Reviews</Link>
+                        </li>
+                        <li className="nav_btn">
+                            <Link to="/signup"><button type="button" className="btn1">Sign Up</button></Link>
+                        </li>
+                        <li className="nav_btn">
+                            <Link to="/login"><button type="button" className="btn1">Login</button></Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
